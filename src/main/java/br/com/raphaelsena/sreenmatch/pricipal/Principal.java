bd18c1a6dfa3f9acab592b0178ab5ced5fabf4e3
Raphael Sena <rsenares1@gmail.com>
2023-12-21 12:43:11 -0300

    Identifying the top 5 episodes

--- src/main/java/br/com/raphaelsena/sreenmatch/pricipal/Principal.java
+++ src/main/java/br/com/raphaelsena/sreenmatch/pricipal/Principal.java
@@ -7,8 +7,10 @@ import br.com.raphaelsena.sreenmatch.service.ConsumoApi;
 import br.com.raphaelsena.sreenmatch.service.ConverteDados;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class Principal {
 
@@ -34,15 +36,18 @@ public class Principal {
 		}
 		temporadas.forEach(System.out::println);
 
-//        for (int i = 0; i < dados.totalTemporadas(); i++) {
-//            List<DadosEpisodios> episodiosTemporada = temporadas.get(i).episodios();
-//
-//            for (int j = 0; j < episodiosTemporada.size(); j++) {
-//                System.out.println(episodiosTemporada.get(j).titulo());
-//            }
-//        }
-
         temporadas.forEach(t -> t.episodios().forEach(e -> System.out.println(e.titulo())));
         temporadas.forEach(System.out::println);
+
+        List<DadosEpisodios> dadosEpisodios = temporadas.stream()
+                .flatMap(t -> t.episodios().stream())
+                .collect(Collectors.toList());
+
+        System.out.println("\nTop 5 episódios: ");
+        dadosEpisodios.stream()
+                .filter(e -> !e.avaliacao().equalsIgnoreCase("N/A"))
+                .sorted(Comparator.comparing(DadosEpisodios::avaliacao).reversed())
+                .limit(5)
+                .forEach(System.out::println);
     }
 }
